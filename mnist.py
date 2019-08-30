5df11a2cb53342a05810d82c292d0d76fc4b42dc
RyanHopkins7 <ryanalexanderhopkins@gmail.com>
2019-08-30 08:05:13 -0700

    Replaced class with createModel function

--- mnist.py
+++ mnist.py
@@ -3,47 +3,43 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-class Model:
-    def __init__(self, fashionOrDigits):
-        pass
-    
-    def getFashionData(self):
-        fashion_mnist = keras.datasets.fashion_mnist
-        return fashion_mnist.load_data()
-    
-    def getDigitData(self):
+
+def createModel(fashionOrDigits):
+    if fashionOrDigits.upper() == "FASHION":
+        fashion_mnist = tf.keras.datasets.fashion_mnist
+        (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+        class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+            'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    elif fashionOrDigits.upper == "DIGIT":
         digit_mnist = tf.keras.datasets.mnist
-        return digit_mnist.load_data()
+        (train_images, train_labels), (test_images, test_labels) = digit_mnist.load_data()
+    else:
+        print("Invalid function input")
+        return
+        
+    train_images = train_images / 255.0
+    test_images = test_images / 255.0
+        
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(28, 28)),
+        keras.layers.Dense(128, activation=tf.nn.relu),
+        keras.layers.Dense(64, activation=tf.nn.relu),
+        keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+    
+    model.compile(optimizer='adam',
+            loss='sparse_categorical_crossentropy',
+            metrics=['accuracy'])
+    
+    model.fit(train_images, train_labels, epochs=6)
+
+    test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+    print('Test accuracy on ', fashionOrDigits, ' MNIST:', test_acc)
+
+    predictions = model.predict(test_images_fashion)
 
 
-(train_images_digits, train_labels_digits), (test_images_digits, test_labels_digits) = mnist.load_data()
-
-class_names_fashion = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-
-train_images_fashion = train_images_fashion / 255.0
-test_images_fashion = test_images_fashion / 255.0
-train_images_digits = train_images_digits / 255.0
-test_images_digits = test_images_digits / 255.0
-
-model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),
-    keras.layers.Dense(128, activation=tf.nn.relu),
-    keras.layers.Dense(64, activation=tf.nn.relu),
-    keras.layers.Dense(10, activation=tf.nn.softmax)
-])
-
-model.compile(optimizer='adam',
-             loss='sparse_categorical_crossentropy',
-             metrics=['accuracy'])
-
-model.fit(train_images_fashion, train_labels_fashion, epochs=6)
-
-test_loss, test_acc = model.evaluate(test_images_fashion, test_labels_fashion)
-
-print('Test accuracy on Fashion MNIST:', test_acc)
-
-predictions = model.predict(test_images_fashion)
 
 
 
