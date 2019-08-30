1e666c6a94a03951bf898298784d04000d9da482
RyanHopkins7 <ryanalexanderhopkins@gmail.com>
2019-08-30 12:18:40 -0700

    Added code for visualization

--- mnist.py
+++ mnist.py
@@ -10,9 +10,11 @@ def createModel(fashionOrDigits):
         (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
         class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
             'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-    elif fashionOrDigits.upper == "DIGITS":
+    elif fashionOrDigits.upper() == "DIGITS":
         digit_mnist = tf.keras.datasets.mnist
         (train_images, train_labels), (test_images, test_labels) = digit_mnist.load_data()
+        class_names = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 
+            'Six', 'Seven', 'Eight', 'Nine']
     else:
         print("Invalid function input")
         return
@@ -40,7 +42,48 @@ def createModel(fashionOrDigits):
     print('Test accuracy on ', fashionOrDigits, ' MNIST:', test_acc)
 
     predictions = model.predict(test_images)
+    
+    num_rows = 5
+    num_cols = 3
+    num_images = num_rows*num_cols
+    plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+    for i in range(num_images):
+        plt.subplot(num_rows, 2*num_cols, 2*i+1)
+        plot_image(i, predictions, test_labels, test_images, class_names)
+        plt.subplot(num_rows, 2*num_cols, 2*i+2)
+        plot_value_array(i, predictions, test_labels)
+    plt.show()
+    
+def plot_image(i, predictions_array, true_label, img, class_names):
+  predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  
+  plt.imshow(img, cmap=plt.cm.binary)
+  
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+  
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
 
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array[i], true_label[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+  
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
 
 createModel("fashion")
 createModel("digits")
